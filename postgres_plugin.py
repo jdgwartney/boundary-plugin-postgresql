dbe6c61c7f4174bf2120fba32bf17abbf4f74133
leefarrar <leefarrar@lees-MacBook-Pro.local>
2014-08-29 22:34:59 +0100

    added lock metrics

--- postgres_plugin.py
+++ postgres_plugin.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 import sys
+import json
 from postgresql import PgInfo
 
 #grab connection info's. This is expected in the Format host/port/db/user/pwd
+_source=sys.argv[6]
 _host = sys.argv[1]
 _port = sys.argv[2]
 _database = sys.argv[3]
@@ -13,15 +15,23 @@ def poll():
 	_dbconn = PgInfo(_host, _port, _database, _user, _password)
 	dbs=_dbconn.getDatabases()
 	#global stats
-	dbStats =_dbconn.getDatabaseStats()
+	
 	writerStats=_dbconn.getBgWriterStats()
 	dbLocks= _dbconn.getLockStatsMode()
 
 	#per db stats
+	dbStats =_dbconn.getDatabaseStats()
 	locksByDB=_dbconn.getLockStatsDB()
 	connStats=_dbconn.getConnectionStats()
-
-	print "LF_PSQL_DBCount ", len(dbs), _host
+	
+	#print dbLocks
+	print "LF_PSQL_EXLocks ", dbLocks['all']['Exclusive'], _source
+	print "LF_PSQL_RowExclusive",  dbLocks['all']['RowExclusive'], _source
+	print "LF_PSQL_ShareRowExclusive", dbLocks['all']['ShareRowExclusive'], _source
+	print "LF_PSQL_ShareUpdateExclusive", dbLocks['all']['ShareUpdateExclusive'], _source
+	print "LF_PSQL_Share", dbLocks['all']['Share'], _host
+	print "LF_PSQL_AccessShare", dbLocks['all']['AccessShare'], _source
+	
 
 poll()
 
