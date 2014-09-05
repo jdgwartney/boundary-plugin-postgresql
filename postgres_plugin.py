938c9822c7cf3a1c7830a271aae4af3542ecfafb
leefarrar <leefarrar@lees-MacBook-Pro.local>
2014-09-05 17:25:47 +0100

    MOAR METRIC

--- postgres_plugin.py
+++ postgres_plugin.py
@@ -14,31 +14,53 @@ _password = sys.argv[5]
 def poll():       
 	_dbconn = PgInfo(_host, _port, _database, _user, _password)
 	dbs=_dbconn.getDatabases()
-	#global stats
 	
+	#global stats
 	writerStats=_dbconn.getBgWriterStats()
 	dbLocks= _dbconn.getLockStatsMode()
-
-	#per db stats
-	dbStats =_dbconn.getDatabaseStats()
-	locksByDB=_dbconn.getLockStatsDB()
-	connStats=_dbconn.getConnectionStats()
+	dbStats =_dbconn.getDatabaseStats() #also gives individual DB stats
 	
-	#print dbLocks
+	#per db stats - not used in this version
+	#locksByDB=_dbconn.getLockStatsDB()
+	#connStats=_dbconn.getConnectionStats()
+	
+	#print writerStats
+	#lock stats
 	print "LF_PSQL_EXLocks ", dbLocks['all']['Exclusive'], _source
 	print "LF_PSQL_RowExclusive",  dbLocks['all']['RowExclusive'], _source
 	print "LF_PSQL_ShareRowExclusive", dbLocks['all']['ShareRowExclusive'], _source
 	print "LF_PSQL_ShareUpdateExclusive", dbLocks['all']['ShareUpdateExclusive'], _source
-	print "LF_PSQL_Share", dbLocks['all']['Share'], _host
+	print "LF_PSQL_Share", dbLocks['all']['Share'], _source
 	print "LF_PSQL_AccessShare", dbLocks['all']['AccessShare'], _source
 	
+	#checkpoint/bgwriter stats
+	print "LF_PSQL_ChkPnt_Wrt_Time", writerStats['checkpoint_write_time'], _source
+	print "LF_PSQL_ChkPnts_Timed", writerStats['checkpoints_timed'], _source
+	print "LF_PSQL_Buffers_Alloc", writerStats['buffers_alloc'], _source
+	print "LF_PSQL_Buffers_Clean", writerStats['buffers_clean'], _source
+	print "LF_PSQL_Buffers_Backnd_fSync", writerStats['buffers_backend_fsync'], _source
+	print "LF_PSQL_ChkPnt_Sync_Time", writerStats['checkpoint_sync_time'], _source
+	print "LF_PSQL_ChkPnts_Req", writerStats['checkpoints_req'], _source
+	print "LF_PSQL_Buffers_Backend", writerStats['buffers_backend'], _source
+	print "LF_PSQL_MaxWritten_Clean", writerStats['maxwritten_clean'], _source
+	print "LF_PSQL_Buffers_ChkPnt", writerStats['buffers_checkpoint'], _source
 
+	#Global DB Stats
+	print "LF_PSQL_blks_read", dbStats['totals']['blks_read'], _source
+	print "LF_PSQL_disk_size", dbStats['totals']['disk_size'], _source
+	print "LF_PSQL_xact_commit", dbStats['totals']['xact_commit'], _source
+	print "LF_PSQL_tup_deleted", dbStats['totals']['tup_deleted'], _source
+	print "LF_PSQL_xact_rollback", dbStats['totals']['xact_rollback'], _source
+	print "LF_PSQL_blks_hit", dbStats['totals']['blks_hit'], _source
+	print "LF_PSQL_tup_returned", dbStats['totals']['tup_returned'], _source
+	print "LF_PSQL_tup_fetched", dbStats['totals']['tup_fetched'], _source
+	print "LF_PSQL_tup_updated", dbStats['totals']['tup_updated'], _source
+	print "LF_PSQL_tup_inserted", dbStats['totals']['tup_inserted'], _source
+	print "LF_PSQL_tup_fetched", dbStats['totals']['tup_fetched'], _source
+
+	
+	
 poll()
 
-"""_host = "127.0.0.1"
-_port = "5432"
-_database = "postgres"
-_user = "leefarrar"
-_password = "Password1"
-"""
+
 
