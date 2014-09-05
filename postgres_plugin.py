17341b2ba42f1d1b34c4717cacd4b961e53dcf5b
leefarrar <leefarrar@lees-MacBook-Pro.local>
2014-09-05 17:47:27 +0100

    changed metric names

--- postgres_plugin.py
+++ postgres_plugin.py
@@ -26,37 +26,37 @@ def poll():
 	
 	#print writerStats
 	#lock stats
-	print "LF_PSQL_EXLocks ", dbLocks['all']['Exclusive'], _source
-	print "LF_PSQL_RowExclusive",  dbLocks['all']['RowExclusive'], _source
-	print "LF_PSQL_ShareRowExclusive", dbLocks['all']['ShareRowExclusive'], _source
-	print "LF_PSQL_ShareUpdateExclusive", dbLocks['all']['ShareUpdateExclusive'], _source
-	print "LF_PSQL_Share", dbLocks['all']['Share'], _source
-	print "LF_PSQL_AccessShare", dbLocks['all']['AccessShare'], _source
+	print "PSQL_EXLocks ", dbLocks['all']['Exclusive'], _source
+	print "PSQL_RowExclusive",  dbLocks['all']['RowExclusive'], _source
+	print "PSQL_ShareRowExclusive", dbLocks['all']['ShareRowExclusive'], _source
+	print "PSQL_ShareUpdateExclusive", dbLocks['all']['ShareUpdateExclusive'], _source
+	print "PSQL_Share", dbLocks['all']['Share'], _source
+	print "PSQL_AccessShare", dbLocks['all']['AccessShare'], _source
 	
 	#checkpoint/bgwriter stats
-	print "LF_PSQL_ChkPnt_Wrt_Time", writerStats['checkpoint_write_time'], _source
-	print "LF_PSQL_ChkPnts_Timed", writerStats['checkpoints_timed'], _source
-	print "LF_PSQL_Buffers_Alloc", writerStats['buffers_alloc'], _source
-	print "LF_PSQL_Buffers_Clean", writerStats['buffers_clean'], _source
-	print "LF_PSQL_Buffers_Backnd_fSync", writerStats['buffers_backend_fsync'], _source
-	print "LF_PSQL_ChkPnt_Sync_Time", writerStats['checkpoint_sync_time'], _source
-	print "LF_PSQL_ChkPnts_Req", writerStats['checkpoints_req'], _source
-	print "LF_PSQL_Buffers_Backend", writerStats['buffers_backend'], _source
-	print "LF_PSQL_MaxWritten_Clean", writerStats['maxwritten_clean'], _source
-	print "LF_PSQL_Buffers_ChkPnt", writerStats['buffers_checkpoint'], _source
+	print "PSQL_ChkPnt_Wrt_Time", writerStats['checkpoint_write_time'], _source
+	print "PSQL_ChkPnts_Timed", writerStats['checkpoints_timed'], _source
+	print "PSQL_Buffers_Alloc", writerStats['buffers_alloc'], _source
+	print "PSQL_Buffers_Clean", writerStats['buffers_clean'], _source
+	print "PSQL_Buffers_Backnd_fSync", writerStats['buffers_backend_fsync'], _source
+	print "PSQL_ChkPnt_Sync_Time", writerStats['checkpoint_sync_time'], _source
+	print "PSQL_ChkPnts_Req", writerStats['checkpoints_req'], _source
+	print "PSQL_Buffers_Backend", writerStats['buffers_backend'], _source
+	print "PSQL_MaxWritten_Clean", writerStats['maxwritten_clean'], _source
+	print "PSQL_Buffers_ChkPnt", writerStats['buffers_checkpoint'], _source
 
 	#Global DB Stats
-	print "LF_PSQL_blks_read", dbStats['totals']['blks_read'], _source
-	print "LF_PSQL_disk_size", dbStats['totals']['disk_size'], _source
-	print "LF_PSQL_xact_commit", dbStats['totals']['xact_commit'], _source
-	print "LF_PSQL_tup_deleted", dbStats['totals']['tup_deleted'], _source
-	print "LF_PSQL_xact_rollback", dbStats['totals']['xact_rollback'], _source
-	print "LF_PSQL_blks_hit", dbStats['totals']['blks_hit'], _source
-	print "LF_PSQL_tup_returned", dbStats['totals']['tup_returned'], _source
-	print "LF_PSQL_tup_fetched", dbStats['totals']['tup_fetched'], _source
-	print "LF_PSQL_tup_updated", dbStats['totals']['tup_updated'], _source
-	print "LF_PSQL_tup_inserted", dbStats['totals']['tup_inserted'], _source
-	print "LF_PSQL_tup_fetched", dbStats['totals']['tup_fetched'], _source
+	print "PSQL_blks_read", dbStats['totals']['blks_read'], _source
+	print "PSQL_disk_size", dbStats['totals']['disk_size'], _source
+	print "PSQL_xact_commit", dbStats['totals']['xact_commit'], _source
+	print "PSQL_tup_deleted", dbStats['totals']['tup_deleted'], _source
+	print "PSQL_xact_rollback", dbStats['totals']['xact_rollback'], _source
+	print "PSQL_blks_hit", dbStats['totals']['blks_hit'], _source
+	print "PSQL_tup_returned", dbStats['totals']['tup_returned'], _source
+	print "PSQL_tup_fetched", dbStats['totals']['tup_fetched'], _source
+	print "PSQL_tup_updated", dbStats['totals']['tup_updated'], _source
+	print "PSQL_tup_inserted", dbStats['totals']['tup_inserted'], _source
+	print "PSQL_tup_fetched", dbStats['totals']['tup_fetched'], _source
 
 	
 	
--- readme.md
+++ readme.md
@@ -1,2 +1,38 @@
- curl https://api.graphdat.com/v1/plugins/private/cpu_core/leefarrar/BPPostgreSQLPlugin
-  -X PUT -u lee@boundary.com:**
+PostgreSQL plugin for Boundary Premium
+Requires Python, PsycoPG2 module (not currently auto installed)
+
+To limit additional metrics the plugin currently provides the below metrics but can easily be extended to provide per DB metrics
+(see code comments) Or make custom collections if required.
+
+Currently provides these metrics:
+Lock Stats:
+PSQL_EXLocks
+PSQL_RowExclusive
+PSQL_ShareRowExclusive
+PSQL_ShareUpdateExclusive
+PSQL_Share
+PSQL_AccessShare
+
+Checkpoint/BGWriter stats:
+PSQL_ChkPnt_Wrt_Time
+PSQL_ChkPnts_Timed
+PSQL_Buffers_Alloc
+PSQL_Buffers_Clean
+PSQL_Buffers_Backnd_fSync
+PSQL_ChkPnt_Sync_Time
+PSQL_ChkPnts_Req
+PSQL_Buffers_Backend
+PSQL_MaxWritten_Clean
+
+Global DB Stats
+PSQL_Buffers_ChkPnt
+PSQL_blks_read
+PSQL_disk_size
+PSQL_xact_commit
+PSQL_tup_deleted
+PSQL_xact_rollback
+PSQL_blks_hit
+PSQL_tup_returned
+PSQL_tup_fetched
+PSQL_tup_updated
+PSQL_tup_inserted
