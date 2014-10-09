1e360a472aa9c986201ddc2b4533f11b284d65bf
jdgwartney <davidg@boundary.com>
2014-10-09 06:16:46 -0700

    Temporarily move readme.md

--- postgres_plugin.py
+++ postgres_plugin.py
@@ -4,21 +4,21 @@ import json
 from postgresql import PgInfo
 
 #grab connection info's. This is expected in the Format host/port/db/user/pwd
-_source=sys.argv[6]
 _host = sys.argv[1]
 _port = sys.argv[2]
 _database = sys.argv[3]
 _user = sys.argv[4]
 _password = sys.argv[5]
+_source = sys.argv[6]
 
 def poll():       
 	_dbconn = PgInfo(_host, _port, _database, _user, _password)
-	dbs=_dbconn.getDatabases()
+	dbs =_dbconn.getDatabases()
 	
 	#global stats
-	writerStats=_dbconn.getBgWriterStats()
-	dbLocks= _dbconn.getLockStatsMode()
-	dbStats =_dbconn.getDatabaseStats() #also gives individual DB stats
+	writerStats = _dbconn.getBgWriterStats()
+	dbLocks = _dbconn.getLockStatsMode()
+	dbStats = _dbconn.getDatabaseStats() #also gives individual DB stats
 	
 	#per db stats - not used in this version
 	#locksByDB=_dbconn.getLockStatsDB()
@@ -26,37 +26,37 @@ def poll():
 	
 	#print writerStats
 	#lock stats
-	print "PSQL_EXLocks ", dbLocks['all']['Exclusive'], _source
-	print "PSQL_RowExclusive",  dbLocks['all']['RowExclusive'], _source
-	print "PSQL_ShareRowExclusive", dbLocks['all']['ShareRowExclusive'], _source
-	print "PSQL_ShareUpdateExclusive", dbLocks['all']['ShareUpdateExclusive'], _source
-	print "PSQL_Share", dbLocks['all']['Share'], _source
-	print "PSQL_AccessShare", dbLocks['all']['AccessShare'], _source
+	print("POSTGRESQL_EXCLUSIVE_LOCKS", dbLocks['all']['Exclusive'], _source
+	print "POSTGRESQL_ROW_EXCLUSIVE",  dbLocks['all']['RowExclusive'], _source
+	print "POSTGRESQL_SHARE_ROW_EXCLUSIVE", dbLocks['all']['ShareRowExclusive'], _source
+	print "POSTGRESQL_SHARE_UPDATE_EXCLUSIVE", dbLocks['all']['ShareUpdateExclusive'], _source
+	print "POSTGRESQL_SHARE", dbLocks['all']['Share'], _source
+	print "POSTGRESQL_ACCESS_SHARE", dbLocks['all']['AccessShare'], _source
 	
 	#checkpoint/bgwriter stats
-	print "PSQL_ChkPnt_Wrt_Time", writerStats['checkpoint_write_time'], _source
-	print "PSQL_ChkPnts_Timed", writerStats['checkpoints_timed'], _source
-	print "PSQL_Buffers_Alloc", writerStats['buffers_alloc'], _source
-	print "PSQL_Buffers_Clean", writerStats['buffers_clean'], _source
-	print "PSQL_Buffers_Backnd_fSync", writerStats['buffers_backend_fsync'], _source
-	print "PSQL_ChkPnt_Sync_Time", writerStats['checkpoint_sync_time'], _source
-	print "PSQL_ChkPnts_Req", writerStats['checkpoints_req'], _source
-	print "PSQL_Buffers_Backend", writerStats['buffers_backend'], _source
-	print "PSQL_MaxWritten_Clean", writerStats['maxwritten_clean'], _source
-	print "PSQL_Buffers_ChkPnt", writerStats['buffers_checkpoint'], _source
+	print "POSTGRESQL_CHECKPOINT_WRITE_TIME", writerStats['checkpoint_write_time'], _source
+	print "POSTGRESQL_CHECKPOINTS_TIMED", writerStats['checkpoints_timed'], _source
+	print "POSTGRESQL_BUFFERS_ALLOCATED", writerStats['buffers_alloc'], _source
+	print "POSTGRESQL_BUFFERS_CLEAN", writerStats['buffers_clean'], _source
+	print "POSTGRESQL_BUFFERS_BACKEND_FSYNC", writerStats['buffers_backend_fsync'], _source
+	print "POSTGRESQL_CHECK_POINT_SYNCHRONIZATION_TIME", writerStats['checkpoint_sync_time'], _source
+	print "POSTGRESQL_CHECK_POINTS_REQUESTED", writerStats['checkpoints_req'], _source
+	print "POSTGRESQL_BUFFERS_BACKEND", writerStats['buffers_backend'], _source
+	print "POSTGRESQL_MAXIUM_WRITTEN_CLEAN", writerStats['maxwritten_clean'], _source
+	print "POSTGRESQL_BUFFERS_CHECKPOINT", writerStats['buffers_checkpoint'], _source
 
 	#Global DB Stats
-	print "PSQL_blks_read", dbStats['totals']['blks_read'], _source
-	print "PSQL_disk_size", dbStats['totals']['disk_size'], _source
-	print "PSQL_xact_commit", dbStats['totals']['xact_commit'], _source
-	print "PSQL_tup_deleted", dbStats['totals']['tup_deleted'], _source
-	print "PSQL_xact_rollback", dbStats['totals']['xact_rollback'], _source
-	print "PSQL_blks_hit", dbStats['totals']['blks_hit'], _source
-	print "PSQL_tup_returned", dbStats['totals']['tup_returned'], _source
-	print "PSQL_tup_fetched", dbStats['totals']['tup_fetched'], _source
-	print "PSQL_tup_updated", dbStats['totals']['tup_updated'], _source
-	print "PSQL_tup_inserted", dbStats['totals']['tup_inserted'], _source
-	print "PSQL_tup_fetched", dbStats['totals']['tup_fetched'], _source
+	print "POSTGRESQL_BLOCKS_READ", dbStats['totals']['blks_read'], _source
+	print "POSTGRESQL_DISK_SIZE", dbStats['totals']['disk_size'], _source
+	print "POSTGRESQL_XACT_COMMIT", dbStats['totals']['xact_commit'], _source
+	print "POSTGRESQL_TUPLES_DELETED", dbStats['totals']['tup_deleted'], _source
+	print "POSTGRESQL_XACT_ROLLBACK", dbStats['totals']['xact_rollback'], _source
+	print "POSTGRESQL_BLOCKS_HIT", dbStats['totals']['blks_hit'], _source
+	print "POSTGRESQL_TUPLES_RETURNED", dbStats['totals']['tup_returned'], _source
+	print "POSTGRESQL_TUPLES_FETCHED", dbStats['totals']['tup_fetched'], _source
+	print "POSTGRESQL_TUPLES_UPDATED", dbStats['totals']['tup_updated'], _source
+	print "POSTGRESQL_TUPLES_INSERTED", dbStats['totals']['tup_inserted'], _source
+	print "POSTGRESQL_TUPLES_FETCHED", dbStats['totals']['tup_fetched'], _source
 
 	
 	
