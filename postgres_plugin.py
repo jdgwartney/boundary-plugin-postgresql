ddba5fcdb117541366e8b552c33518976ede3d54
jdgwartney <davidg@boundary.com>
2015-04-20 21:16:20 +0000

    Add checking of key in array before dereferencing to avoid an exception

--- postgres_plugin.py
+++ postgres_plugin.py
@@ -45,16 +45,27 @@ def poll():
 	print("POSTGRESQL_ACCESS_SHARE_LOCKS {0} {1}".format(dbLocks['all']['AccessShare'], _source))
 	
 	#checkpoint/bgwriter stats
-	print("POSTGRESQL_CHECKPOINT_WRITE_TIME {0} {1}".format(writerStats['checkpoint_write_time'], _source))
-	print("POSTGRESQL_CHECKPOINTS_TIMED {0} {1}".format(writerStats['checkpoints_timed'], _source))
-	print("POSTGRESQL_BUFFERS_ALLOCATED {0} {1}".format(writerStats['buffers_alloc'], _source))
-	print("POSTGRESQL_BUFFERS_CLEAN {0} {1}".format(writerStats['buffers_clean'], _source))
-	print("POSTGRESQL_BUFFERS_BACKEND_FSYNC {0} {1}".format(writerStats['buffers_backend_fsync'], _source))
-	print("POSTGRESQL_CHECKPOINT_SYNCHRONIZATION_TIME {0} {1}".format(writerStats['checkpoint_sync_time'], _source))
-	print("POSTGRESQL_CHECKPOINTS_REQUESTED {0} {1}".format(writerStats['checkpoints_req'], _source))
-	print("POSTGRESQL_BUFFERS_BACKEND {0} {1}".format(writerStats['buffers_backend'], _source))
-	print("POSTGRESQL_MAXIMUM_WRITTEN_CLEAN {0} {1}".format(writerStats['maxwritten_clean'], _source))
-	print("POSTGRESQL_BUFFERS_CHECKPOINT {0} {1}".format(writerStats['buffers_checkpoint'], _source))
+        # Check if the key exists before deferencing not all versions of PostgreSQL contains the same stats
+	if 'checkpoint_write_time' in writerStats:
+		print("POSTGRESQL_CHECKPOINT_WRITE_TIME {0} {1}".format(writerStats['checkpoint_write_time'], _source))
+	if 'checkpoints_timed' in writerStats:
+		print("POSTGRESQL_CHECKPOINTS_TIMED {0} {1}".format(writerStats['checkpoints_timed'], _source))
+	if 'buffers_alloc' in writerStats:
+		print("POSTGRESQL_BUFFERS_ALLOCATED {0} {1}".format(writerStats['buffers_alloc'], _source))
+	if 'buffers_clean' in writerStats:
+		print("POSTGRESQL_BUFFERS_CLEAN {0} {1}".format(writerStats['buffers_clean'], _source))
+	if 'buffers_backend_fsync' in writerStats:
+		print("POSTGRESQL_BUFFERS_BACKEND_FSYNC {0} {1}".format(writerStats['buffers_backend_fsync'], _source))
+	if 'checkpoint_sync_time' in writerStats:
+		print("POSTGRESQL_CHECKPOINT_SYNCHRONIZATION_TIME {0} {1}".format(writerStats['checkpoint_sync_time'], _source))
+	if 'checkpoints_req' in writerStats:
+		print("POSTGRESQL_CHECKPOINTS_REQUESTED {0} {1}".format(writerStats['checkpoints_req'], _source))
+	if 'buffers_backend' in writerStats:
+		print("POSTGRESQL_BUFFERS_BACKEND {0} {1}".format(writerStats['buffers_backend'], _source))
+	if 'maxwritten_clean' in writerStats:
+		print("POSTGRESQL_MAXIMUM_WRITTEN_CLEAN {0} {1}".format(writerStats['maxwritten_clean'], _source))
+	if 'buffers_checkpoint' in writerStats:
+		print("POSTGRESQL_BUFFERS_CHECKPOINT {0} {1}".format(writerStats['buffers_checkpoint'], _source))
 
 	#Global DB Stats
 	print("POSTGRESQL_BLOCKS_READ {0} {1}".format(dbStats['totals']['blks_read'], _source))
@@ -71,6 +82,6 @@ def poll():
 	sys.stdout.flush()
 while True:
 	poll()
-	time.sleep(1)
+ 	time.sleep(1)
 
 
